28cbb4026ce5d14d927e0c12ae200189363c56d6
robae2821 <89492065+robae2821@users.noreply.github.com>
2021-11-08 13:40:00 -0700

    change index

---
+++ app.js
@@ -0,0 +1,138 @@
+// alerts message on the page
+alert("you have to be 18 to enter this Site");
+// ask a yes/no
+confirm("Are You Over 18?")
+// give an Input. prompys an answer.
+prompt("How Old Are You?")
+
+
+var firstName="Robel";
+var lastName="Medhanie";
+var middileInitial="Z";
+// console.log, aleret, document.write, target html element
+console.log(firstName[5])
+
+
+alert("My Name Is " + " " + firstName + '\n' +
+ "My Last name is" + " " + lastName + '\n' +
+  "My Last Name Is " + " " + lastName);
+
+
+
+console.log("My Name Is " + " " + firstName + '\n' +
+"My Last name is" + " " + lastName + '\n' + 
+"My Last Name Is " + " " + lastName);
+
+
+
+
+
+document.write("My Name Is " + " " + firstName + '\n' + 
+"My Last name is" + " " + lastName + '\n' + 
+"My Last Name Is " + " " + lastName);
+
+
+
+
+document.getElementById("demo").innerHTML="My Name Is " + " " + firstName + '\n' +
+ "My Last name is" + " " + lastName + '\n' + 
+ "My Last Name Is " + " " + lastName;
+
+
+ // The Fortune Teller
+
+var numberOfChildren= 7;
+var partnersName= "DD?";
+var geographicLocation= "1356 peoria st";
+var jobTitle= "Truck driver";
+
+function myFunction() {
+  var message="Hello World";
+  document.getElementById("memo").innerHTML=message;
+}
+
+// console.log, aleret, document.write, target html element;
+
+// alert("My Name Is " + " " + firstName + '\n' +
+//  "Marrid with" + " " + partnersName + '\n' +
+//   "with kids " + " " + numberOfChildren);
+
+  console.log("My Name Is " + " " + firstName + '\n' +
+ "Marrid with " + " " + partnersName + '\n' +
+  "With Kids " + " " + numberOfChildren);
+
+  document.write("My Name Is " + " " + firstName + '\n' +
+ "Marrid with " + " " + partnersName + '\n' +
+  "with kids " + " " + numberOfChildren);
+
+  document.getElementById("memo").innerHTML="My Name Is " + " " + firstName + '\n' +
+ "Marrid with " + " " + partnersName + '\n' +
+  "with kids " + " " + numberOfChildren;
+
+
+
+    // console.log("Hello World");
+  // // window objects
+  
+  // // alerts message on the page
+  // alert("you have to be 18 to enter this Site");
+  // // ask a yes/no
+  // confirm("Are You Over 18?")
+  // // give an Input. prompys an answer.
+  // prompt("How Old Are You?")
+  
+  
+  var firstName= "Robel";
+  var pizzaCount= 8;
+  var isHungery="true"
+  var totalCoast= 8*25 + "$";
+  
+//   console.log, aleret, document.write, target.html-element;
+  
+  alert("My Name Is " + " " + firstName + '\n' +
+  "Pizza count " + " " + pizzaCount + '\n' +
+  "Is Hungery" + " " + isHungery + '\n' +
+  "Your Total Coast Is" + " " + totalCoast);
+  
+    console.log("My Name Is " + " " + firstName + '\n' +
+    "Pizza count Is" + " " + pizzaCount + '\n' +
+    "Is Hungery" + " " + isHungery + '\n' +
+    "Your Total Coast Is" + " " + totalCoast);
+  
+    document.write("My Name Is " + " " + firstName + '\n' +
+    "Pizza count Is" + " " + pizzaCount + '\n' +
+    "Is Hungery" + " " + isHungery + '\n' +
+    "Your Total Coast Is" + " " + totalCoast);
+  
+    document.getElementById("js").innerHTML="My Name Is " + " " + firstName + '\n' +
+  "Pizza count Is" + " " + pizzaCount + '\n' +
+  "Is Hungery" + " " + isHungery + '\n' +
+  "Your Total Coast Is" + " " + totalCoast;
+
+
+  // //   The Geometrizer
+// // Create 2 functions that calculate properties of a circle, using the definitions here.
+// // Create a function called calcCircumfrence:
+// // Pass the radius to the function.
+// // Calculate the circumference based on the radius, and output "The circumference is NN".
+// // Create a function called calcArea:
+// // Pass the radius to the function.
+// // Calculate the area based on the radius, and output "The area is NN".
+
+function cCir (radius) {
+    var cirC = Math.PI * 2 * radius;
+    console.log("The circumference is" + cirC);
+  }
+  
+  cCir (6)
+  // Area
+  function calA (radius) {
+    var cirA = Math.PI * radius * radius;
+    console.log("The Area Is " + cirA);
+  
+  }
+  
+  calA (7)
+
+
+
